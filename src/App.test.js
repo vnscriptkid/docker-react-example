659c59524e110c80a5bf45d632e8ebf57d94fd27
Thanh Nguyen <vnscriptkid@gmail.com>
2019-06-15 17:37:37 +0700

    fix test

--- src/App.test.js
+++ src/App.test.js
@@ -12,23 +12,5 @@ it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
-});
-
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
-
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
-
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-  expect(1).toEqual(2);
+  expect(1 + 1).toEqual(2);
 });
